abae3b21b385a5e9f43cd31f1b53cc2e8ffa39fc
Generic404_ <genericplaceholder0@gmail.com>
2025-09-01 20:06:16 -0700

    Initial commit

---
+++ common/src/main/java/net/generic404_/selfalchemy/Constants.java
@@ -0,0 +1,11 @@
+package net.generic404_.selfalchemy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "selfalchemy";
+    public static final String MOD_NAME = "Self Alchemy";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ forge/src/main/java/net/generic404_/selfalchemy/SelfAlchemy.java
@@ -0,0 +1,18 @@
+package net.generic404_.selfalchemy;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class SelfAlchemy {
+
+    public SelfAlchemy() {
+        // This method is invoked by the Forge mod loader when it is ready
+        // to load your mod. You can access Forge and Common code in this
+        // project.
+
+        // Use Forge to bootstrap the Common mod.
+        Constants.LOG.info("Hello Forge world!");
+        CommonClass.init();
+
+    }
+}
